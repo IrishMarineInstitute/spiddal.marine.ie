5f35c5f14e2e110379854214ac3140d081383af1
dmuser <fullergalway@gmail.com>
2015-09-30 14:06:44 +0100

    support archive files and cat files in the nightly compress

--- bin/daily_compress.sh
+++ bin/daily_compress.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 DATA_DIR="/home/dmuser/sites/spiddal.marine.ie/html/data"
-TYPES="hydrophones ctds fluorometers"
+TAR_TYPES="hydrophones"
+CAT_TYPES="ctds fluorometers"
 hydrophones="SBF1323"
-ctds="SPIDCTD"
-fluorometers="SPIDFLNTU"
+ctds="I-OCEAN7-304-XXXX"
+fluorometers="WL-ECO-FLNTU-3137"
 DIR_DATE=$(date -d "yesterday 13:00 " '+%Y/%m/%d')
 FILE_DATE=$(date -d "yesterday 13:00 " '+%Y%m%d')
-for TYPE in $(echo $TYPES);
+for TYPE in $(echo $TAR_TYPES);
   do for DEVICE in $(eval echo \$${TYPE});
    do FNAME=${DEVICE}_${FILE_DATE};
      cd $DATA_DIR/$TYPE/$DEVICE/$DIR_DATE && tar cfz ${FNAME}.tgz *.txt && zip  -q ${FNAME}  *.txt
+  done
+done
+for TYPE in $(echo $CAT_TYPES);
+  do for DEVICE in $(eval echo \$${TYPE});
+   do FNAME=${DEVICE}_${FILE_DATE};
+     cd $DATA_DIR/$TYPE/$DEVICE/$DIR_DATE && rm -f ${FNAME}.txt && cat *.txt > ${FNAME}.txt
   done
 done
