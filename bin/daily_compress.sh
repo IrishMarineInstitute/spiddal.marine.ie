9ecc1820103c8614966120aa1b6488a9448cb5d8
dmuser <fullergalway@gmail.com>
2015-11-05 17:03:28 +0000

    added acoustic_telemetry

--- bin/daily_compress.sh
+++ bin/daily_compress.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 DATA_DIR="/home/dmuser/sites/spiddal.marine.ie/html/data"
 TAR_TYPES="hydrophones"
-CAT_TYPES="ctds fluorometers"
+CAT_TYPES="ctds fluorometers acoustic_telemetry"
 hydrophones="SBF1323"
 ctds="I-OCEAN7-304-XXXX"
 fluorometers="WL-ECO-FLNTU-3137"
+accoustic_telemetry="VMVR2C450117"
 DIR_DATE=$(date -d "yesterday 13:00 " '+%Y/%m/%d')
 FILE_DATE=$(date -d "yesterday 13:00 " '+%Y%m%d')
 for TYPE in $(echo $TAR_TYPES);
