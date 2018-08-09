c582dbc08ac0e5c16d58a6212db6efebf0efec90
Rob Fuller <fullergalway@gmail.com>
2018-08-09 16:13:31 +0100

    updated devices

--- bin/device_files.sh
+++ bin/device_files.sh
@@ -3,11 +3,11 @@ DATA_DIR="/home/dmuser/sites/spiddal.marine.ie/html/data"
 TAR_TYPES="hydrophones"
 CAT_TYPES="ctds fluorometers acoustic_telemetry adcps"
 MONTHLY_CAT_TYPES="adcps"
-hydrophones="SBF1622"
-ctds="I-OCEAN7-304-0616641"
-fluorometers="WL-ECO-FLNTU-4476"
+hydrophones="SBF1323"
+ctds="I-OCEAN7-304-1214551"
+fluorometers="WL-ECO-FLNTU-3137"
 acoustic_telemetry="VMVR2C450218"
-adcps="TRDI-WHB600Hz-1323"
+adcps="TRDI-WHB600Hz-10488"
 # file extensions
 ext_hydrophones=.txt
 ext_ctds=.txt
